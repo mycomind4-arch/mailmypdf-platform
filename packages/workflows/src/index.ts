fc71e5796177b315fd4b93f483517b61bf5922da
mycomind4-arch <mycomind4@gmail.com>
2026-08-21 23:47:41 -0700

    Export workflow factory registries

--- packages/workflows/src/index.ts
+++ packages/workflows/src/index.ts
@@ -1,3 +1,7 @@
 export * from "./gold-standard-pipeline.js";
 export * from "./domain-pack-contract.js";
 export * from "./ecosystem-certification.js";
+export * from "./pipeline-registry.js";
+export * from "./adapter-registry.js";
+export * from "./workflow-manifest.js";
+export * from "./workflow-factory.js";
