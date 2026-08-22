7c6317c5fd2ef860379de5db958913c402b29699
mycomind4-arch <mycomind4@gmail.com>
2026-08-21 23:49:35 -0700

    Export shared capability registry

--- packages/workflows/src/index.ts
+++ packages/workflows/src/index.ts
@@ -4,6 +4,7 @@ export * from "./ecosystem-certification.js";
 export * from "./pipeline-registry.js";
 export * from "./adapter-registry.js";
 export * from "./pipeline-adapter-matrix.js";
+export * from "./capability-registry.js";
 export * from "./workflow-manifest.js";
 export * from "./workflow-factory.js";
 export * from "./workflow-registry.js";
